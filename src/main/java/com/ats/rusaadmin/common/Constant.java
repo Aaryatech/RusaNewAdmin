4b1e4040e31ebea74b8a32b2104e39989fcb8af8
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-03-07 13:58:26 +0530

    ////akshay

--- src/main/java/com/ats/rusaadmin/common/Constant.java
+++ src/main/java/com/ats/rusaadmin/common/Constant.java
@@ -3,14 +3,18 @@ package com.ats.rusaadmin.common;
 public class Constant {
 	
 	//public static final String url="http://192.168.10.226:8080/RusaWebapi/";
+	//public static final String url="http://tomcat.aaryatechindia.in:6435/atswebapi/"; 
 	public static final String url="http://localhost:8093/";
-	public static final int gallryImage = 1;
+	
+	//Local path------------------------
+	/*public static final int gallryImage = 1;
 	public static final String lgogImageURL = "/home/lenovo/Downloads/apache-tomcat-8.5.37/webapps/media/logo/";
 	public static final String bannerImageURL = "/home/lenovo/Downloads/apache-tomcat-8.5.37/webapps/media/banenr/";
 	public static final String uploadDocURL = "/home/lenovo/Downloads/apache-tomcat-8.5.37/webapps/media/pdf/";
 	public static final String gallryImageURL = "/home/lenovo/Downloads/apache-tomcat-8.5.37/webapps/media/gallery/";
 	public static final String otherDocURL = "/home/lenovo/Downloads/apache-tomcat-8.5.37/webapps/media/other/";
 	public static final String cmsPdf = "/home/lenovo/Downloads/apache-tomcat-8.5.37/webapps/media/pdf/";
+	public static final String userProfileURL = "/home/lenovo/Downloads/apache-tomcat-8.5.37/webapps/media/userprofile/";
 	public static String[] values = {"jpg","jpeg","gif","png"};
 	public static String[] DocValues = {"txt","doc","pdf","xls"};
 	public static String[] DocImgValues = {"txt","doc","pdf","xls","jpg","jpeg","gif","png"};
@@ -23,5 +27,33 @@ public class Constant {
 	public static final String getGallryImageURL = "http://192.168.10.226:8080/media/gallery/";
 	public static final String getCmsPdf = "http://192.168.10.226:8080/media/pdf/";
 	public static final String getOtherDocURL = "http://192.168.10.226:8080/media/other/";
+	public static final String getUserProfileURL = "http://192.168.10.226:8080/media/userprofile/";
+	//public static final String siteAdminUrl = "http://192.168.10.226:8080/RusaAdmin/";
+	public static final String siteAdminUrl = "http://192.168.10.226:8080/RusaAdmin/";*/
+	
+	
+	public static final int gallryImage = 1;
+	public static final String lgogImageURL = "/home/aaryate1/ats.aaryatechindia.in/tomcat-8.0.18/webapps/media/logo/";
+	public static final String bannerImageURL = "/home/aaryate1/ats.aaryatechindia.in/tomcat-8.0.18/webapps/media/banenr/";
+	public static final String uploadDocURL = "/home/aaryate1/ats.aaryatechindia.in/tomcat-8.0.18/webapps/media/pdf/";
+	public static final String gallryImageURL = "/home/aaryate1/ats.aaryatechindia.in/tomcat-8.0.18/webapps/media/gallery/";
+	public static final String otherDocURL = "/home/aaryate1/ats.aaryatechindia.in/tomcat-8.0.18/webapps/media/other/";
+	public static final String cmsPdf = "/home/aaryate1/ats.aaryatechindia.in/tomcat-8.0.18/webapps/media/pdf/";
+	public static final String userProfileURL = "/home/aaryate1/ats.aaryatechindia.in/tomcat-8.0.18/webapps/media/userprofile/";
+	public static String[] values = {"jpg","jpeg","gif","png"};
+	public static String[] DocValues = {"txt","doc","pdf","xls"};
+	public static String[] DocImgValues = {"txt","doc","pdf","xls","jpg","jpeg","gif","png"};
+	public static int mod=0;
+	public static int subMod=0;
+	public static final String logoName = "Russa-Logo";
+	public static final String getLgogImageURL = "http://tomcat.aaryatechindia.in:6435/media/logo/";
+	public static final String getBannerImageURL = "http://tomcat.aaryatechindia.in:6435/media/banenr/";
+	public static final String getUploadDocURL = "http://tomcat.aaryatechindia.in:6435/media/pdf/";
+	public static final String getGallryImageURL = "http://tomcat.aaryatechindia.in:6435/media/gallery/";
+	public static final String getCmsPdf = "http://tomcat.aaryatechindia.in:6435/media/pdf/";
+	public static final String getOtherDocURL = "http://tomcat.aaryatechindia.in:6435/media/other/";
+	public static final String getUserProfileURL = "http://tomcat.aaryatechindia.in:6435/media/userprofile/";
+	//public static final String siteAdminUrl = "http://192.168.10.226:8080/RusaAdmin/";
+	public static final String siteAdminUrl = "http://ats.aaryatechindia.in:6435/ats/";
 	
 }
