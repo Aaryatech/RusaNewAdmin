c9bef9e28b4e2e4739a5a4928100ee19852f5946
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2019-03-19 19:36:57 +0530

    /Akshay

--- src/main/java/com/ats/rusaadmin/common/Constant.java
+++ src/main/java/com/ats/rusaadmin/common/Constant.java
@@ -3,11 +3,11 @@ package com.ats.rusaadmin.common;
 public class Constant {
 	
 	//public static final String url="http://192.168.10.226:8080/RusaWebapi/";
-	//public static final String url="http://tomcat.aaryatechindia.in:6435/atswebapi/"; 
-	public static final String url="http://localhost:8093/";
+	public static final String url="http://tomcat.aaryatechindia.in:6435/atswebapi/"; 
+	//public static final String url="http://localhost:8093/";
 	
 	//Local path------------------------
-	/*public static final int gallryImage = 1;
+	 /*public static final int gallryImage = 1;
 	public static final String lgogImageURL = "/home/lenovo/Downloads/apache-tomcat-8.5.37/webapps/media/logo/";
 	public static final String bannerImageURL = "/home/lenovo/Downloads/apache-tomcat-8.5.37/webapps/media/banenr/";
 	public static final String uploadDocURL = "/home/lenovo/Downloads/apache-tomcat-8.5.37/webapps/media/pdf/";
@@ -21,18 +21,18 @@ public class Constant {
 	public static int mod=0;
 	public static int subMod=0;
 	public static final String logoName = "Russa-Logo";
-	public static final String getLgogImageURL = "http://192.168.10.226:8080/media/logo/";
-	public static final String getBannerImageURL = "http://192.168.10.226:8080/media/banenr/";
-	public static final String getUploadDocURL = "http://192.168.10.226:8080/media/pdf/";
-	public static final String getGallryImageURL = "http://192.168.10.226:8080/media/gallery/";
-	public static final String getCmsPdf = "http://192.168.10.226:8080/media/pdf/";
-	public static final String getOtherDocURL = "http://192.168.10.226:8080/media/other/";
-	public static final String getUserProfileURL = "http://192.168.10.226:8080/media/userprofile/";
+	public static final String getLgogImageURL = "http://192.168.10.227:8080/media/logo/";
+	public static final String getBannerImageURL = "http://192.168.10.227:8080/media/banenr/";
+	public static final String getUploadDocURL = "http://192.168.10.227:8080/media/pdf/";
+	public static final String getGallryImageURL = "http://192.168.10.227:8080/media/gallery/";
+	public static final String getCmsPdf = "http://192.168.10.227:8080/media/pdf/";
+	public static final String getOtherDocURL = "http://192.168.10.227:8080/media/other/";
+	public static final String getUserProfileURL = "http://192.168.10.227:8080/media/userprofile/";
 	//public static final String siteAdminUrl = "http://192.168.10.226:8080/RusaAdmin/";
-	public static final String siteAdminUrl = "http://192.168.10.226:8080/RusaAdmin/";*/
+	public static final String siteAdminUrl = "http://192.168.10.227:8080/RusaAdmin/"; */
 	
 	
-	public static final int gallryImage = 1;
+	 public static final int gallryImage = 1;
 	public static final String lgogImageURL = "/home/aaryate1/ats.aaryatechindia.in/tomcat-8.0.18/webapps/media/logo/";
 	public static final String bannerImageURL = "/home/aaryate1/ats.aaryatechindia.in/tomcat-8.0.18/webapps/media/banenr/";
 	public static final String uploadDocURL = "/home/aaryate1/ats.aaryatechindia.in/tomcat-8.0.18/webapps/media/pdf/";
@@ -54,6 +54,6 @@ public class Constant {
 	public static final String getOtherDocURL = "http://tomcat.aaryatechindia.in:6435/media/other/";
 	public static final String getUserProfileURL = "http://tomcat.aaryatechindia.in:6435/media/userprofile/";
 	//public static final String siteAdminUrl = "http://192.168.10.226:8080/RusaAdmin/";
-	public static final String siteAdminUrl = "http://ats.aaryatechindia.in:6435/ats/";
+	public static final String siteAdminUrl = "http://ats.aaryatechindia.in:6435/ats/"; 
 	
 }
