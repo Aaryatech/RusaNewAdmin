eb5c8d3e055ec2cd4ff525aaabb8a6ff34a3bcb8
lenovo <lenovo@lenovo-Lenovo-ideapad-320-15IKB>
2020-01-23 19:29:12 +0530

    /Akshay

--- src/main/java/com/ats/rusaadmin/XssEscapeUtils.java
+++ src/main/java/com/ats/rusaadmin/XssEscapeUtils.java
@@ -1,5 +1,8 @@
 package com.ats.rusaadmin;
 
+import java.net.URLDecoder;
+
+import org.apache.commons.text.StringEscapeUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
@@ -18,15 +21,53 @@ public class XssEscapeUtils {
 	// a) in all input type text and textarea without ckeditor or any html editor
 	// in both project
 	public static String jsoupParse(String str) {
+		//str = str.replaceAll("\\<.*?\\>", "");
+		
 		str = str.replaceAll("\\<.*?\\>", "");
-		return Jsoup.parse(str).text();
+		str=Jsoup.parse(str).text();
+		//System.err.println("str1 in jsoupParse " +str);
+		//str = URLEncoder.encode(str);
+		try {
+			str =URLDecoder.decode(str,"UTF-8");
+		}catch(Exception e) {
+			
+		}
+		
+		
+	  	str=StringEscapeUtils.escapeHtml4(str);
+	 	//str=StringEscapeUtils.escapeJava(str);
+		//str= StringEscapeUtils.escapeEcmaScripta(str);
+		//str=StringEscapeUtils.unescapeHtml4(str);
+		 
+	//	str=StringEscapeUtils.escapeJava(str); 
+		//System.err.println("str1 in encode " +str);
+		
+		return str;
 	}
 
 	// b) in all textarea with ckeditor or any html editor in both project
 	public static String jsoupParseClean(String str) {
-		return Jsoup.clean(str, Whitelist.relaxed());
+		//Jsoup.clean(str, Whitelist.relaxed());
+		return str;
 	}
 
+	
+	public static String jsoupParseOutput(String str) {
+		//System.err.println("str0 " +str);
+
+//			str = URLEncoder.encode(str);
+//			System.err.println("str1 encode " +str);
+
+
+
+		str=StringEscapeUtils.unescapeHtml4(str);
+		//System.err.println("str2  unescapeHtml4 " +str);
+		str= StringEscapeUtils.escapeEcmaScript(str);
+		str=StringEscapeUtils.escapeJava(str);
+		str=StringEscapeUtils.escapeJava(str);
+		//System.err.println("str3 unescapeJava " +str);
+		return str;
+		}
 	/*
 	 * public static String jsoupParse(String str) { return Jsoup.parse(str).text();
 	 * }
